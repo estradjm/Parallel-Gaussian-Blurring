1d3b67eacd6bc4437405cc420959252bc3f22a9f
Jenniffer Estrada <estradjm@users.noreply.github.com>
2017-06-13 02:25:28 -0600

    Create filter.c

--- src/filter.c
+++ src/filter.c
@@ -6,26 +6,26 @@
 
 
 FILTER *filter_create_avg(int radius) {
-	//Allocate mem for the structure
+	// Allocate memory for the structure
 	FILTER *filter = (FILTER*) malloc(sizeof(FILTER));
 	filter->radius = radius;
 	filter->type = FILTER_AVG;
 
-	//Used for iterations
+	// Used for iterations
 	int i, j;
 
-	//The matrix width and height
+	// The matrix width and height
 	int dim = 2*radius+1;
 
-	//Alocate mem for the matrix
+	// Alocate memory for the matrix
 	filter->matrix = (double**) malloc(dim * sizeof(double*));
 	for(i = 0; i < dim; i++)
 		filter->matrix[i] = (double*) malloc(dim * sizeof(double));
 
-	//The value that every entry in the matrix will contain
+	// The value that every entry in the matrix will contain
 	double avg = 1.0 / (dim * dim);
 
-	//Set the values
+	// Set the values
 	for(i = 0; i < dim; i++)
 		for(j = 0; j < dim; j++)
 			filter->matrix[i][j] = avg;
@@ -33,7 +33,7 @@ FILTER *filter_create_avg(int radius) {
 	return filter;	
 }
 
-/* Private function (local scope only in filter.c)*/
+// Private function (local scope only in filter.c)
 double gauss_2d(int x, int y, double sigma) {
 	double result = 1.0 / (2 * PI * sigma * sigma);
 	result *= exp(-(x*x+y*y)/(2 * sigma * sigma));
@@ -41,23 +41,23 @@ double gauss_2d(int x, int y, double sigma) {
 }
 
 FILTER *filter_create_gauss(int radius, double sigma) {
-	//Allocate mem for the structure
+	// Allocate memory for the structure
 	FILTER *filter = (FILTER*) malloc(sizeof(FILTER));
 	filter->radius = radius;
 	filter->type = FILTER_GAUSS;
 
-	//Used for iterations
+	// Used for iterations
 	int i, j;
 
 	//The matrix width and height
 	int dim = 2*radius+1;
 
-	//Alocate mem for the matrix
+	// Alocate memory for the matrix
 	filter->matrix = (double**) malloc(dim * sizeof(double*));
 	for(i = 0; i < dim; i++)
 		filter->matrix[i] = (double*) malloc(dim * sizeof(double));
 
-	//Calculate
+	// Calculate
 	double sum = 0.0;
 	for(i = -radius; i <= radius; i++)
 		for(j = -radius; j <= radius; j++) {
@@ -65,7 +65,7 @@ FILTER *filter_create_gauss(int radius, double sigma) {
 			sum += filter->matrix[i+radius][j+radius];
 		}
 
-	//Correct so that the sum of all elements ~= 1
+	// Correct so that the sum of all elements ~= 1
 	for(i = 0; i < 2*radius+1; i++)
 		for(j = 0; j < 2*radius+1; j++)
 			filter->matrix[i][j] /= sum;
@@ -84,7 +84,7 @@ void filter_print(FILTER *filter) {
 }
 
 void filter_free(FILTER *filter) {
-	//Free matrix
+	// Free filter matrix
 	int dim=2*filter->radius+1, i;
 	for(i = 0; i < dim; i++)
 		free(filter->matrix[i]);
